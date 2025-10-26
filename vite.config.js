f73e838a23787b4c78d000cd03a3f20ae7346a16
theComputationalGuy <khalidshaikh848685@gmail.com>
2025-10-26 18:57:42 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/REPO/', // replace REPO with your repository name
+  base: '/my-website/', // replace REPO with your repository name
   plugins: [react()],
 });
